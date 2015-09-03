0475c475ea3d430453e0999d100753ba1755ec15
Steven <Steven@10.30.21.157>
2015-09-02 22:06:15 -0500

    First commit

---
+++ src/java/model/WelcomeService.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.util.Calendar;
+
+/**
+ *
+ * @author Steven Endres
+ */
+public class WelcomeService {
+
+    //constructor not used
+//    public WelcomeService() { }
+        
+        private String getTOD(){
+        Calendar currentDate = null;
+        String timeOfDayGreeting = null;
+        String inputName;
+ 
+        
+   
+        int timeOfDay = currentDate.get(Calendar.HOUR_OF_DAY);
+
+        if (timeOfDay <= 12) {
+         timeOfDayGreeting = "Good Morning";
+        } else if (timeOfDay >= 12 && timeOfDay <= 16) {
+            timeOfDayGreeting = "Good Afternoon";
+        } else if (timeOfDay >= 16) {
+            timeOfDayGreeting = "Good Evening";
+        }
+        return timeOfDayGreeting;
+    }
+
+
+    private String createGretting(String inputName) {
+        return (getTOD() + "," + inputName + "." + "  Welcome!");
+
+    }
+
+}
---
+++ web/PageGenerator2.jsp
@@ -0,0 +1,21 @@
+<%-- 
+    Document   : PageGenerator2
+    Created on : Sep 2, 2015, 11:28:34 AM
+    Author     : Steven
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>CARS MPG</h1>
+        <p>FORD 18  24</p>
+        <p>Chevy 18  24</p>
+        <p>DODGE 18  24</p>
+        <p>Servlet PageGenerator at request.getContextPath()</p>;
+    </body>
+</html>
